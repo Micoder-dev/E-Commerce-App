d220f6cfb1ef75e36ce5831f2803dd7d3b1d2270
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-21 21:30:39 +0530

    ecommerce initial update with modified source code

---
+++ app/src/main/java/com/micoder/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.micoder.shopping.Prevalent;
+
+import com.micoder.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
